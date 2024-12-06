bf3931fd84255072f663d20088838072405bf312
BenHerna50 <bherna50@asu.edu>
2024-12-05 21:44:02 -0500

    v

--- java.js
+++ java.js
@@ -1,8 +1,10 @@
 "use strict";
 
-
-  
-  
-  
-  
-  
+function myFunction() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+  }
+  function myFunction() {
+    var element = document.body;
+    element.classList.toggle("light-mode");
+  }  
