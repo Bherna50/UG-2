abfa51597ce966b9ad25a30223dd330f6caf2c97
BenHerna50 <bherna50@asu.edu>
2024-12-05 23:06:36 -0500

    f

--- java.js
+++ java.js
@@ -1,5 +1,6 @@
+//required for project
 "use strict";
-
+//trying to toggle dark/light mode but does not work.
 function myFunction() {
     var element = document.body;
     element.classList.toggle("dark-mode");
@@ -8,3 +9,14 @@ function myFunction() {
     var element = document.body;
     element.classList.toggle("light-mode");
   }
+//trying to use this function to run my variables but can't think of how
+//to simplify or if I should use if else statement? 
+  function getRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+//I believe an array would go here of the correct numbers?
+  }
+  //i know i need to pass my game variables such as guessInput,guess and message.
+  for (let i = 0; i < 5; i++) { 
+    console.log(getRandomNumber(1, 10)); 
+ }
+ //product display was thinking of adding albums to sell or tab books.
